be0cd50791f01e4ca8af12ac04cfaf499b2286db
Ceredron <roarmj@gmail.com>
2018-02-18 13:24:57 +0100

    Added paginator, centered stuff in css

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import styles from './index.css';
 var request = require('request');
 /**
  * To implement
@@ -14,7 +13,9 @@ class MainScreen extends Component {
         this.props = props;
 
         this.state = {
-            companyList: [] // Initializes it as an empty list in order to be able to use .map on it in render()
+            companyList: [], // Initializes it as an empty list in order to be able to use .map on it in render()
+            prev: null, // For paginator
+            next: null, // For paginator
         }
 
         this.incrementalSearch = this.incrementalSearch.bind(this); // Binds it to use it in onChange of search input
@@ -45,7 +46,20 @@ class MainScreen extends Component {
             } else if (res.statusCode !== 200) {
                 console.log('Status:', res.statusCode);
             } else { // If successful, update state accordingly
-                this.setState({companyList: data.data == null?[]:data.data}); // Question mark conditional used in case search returns no results
+                var prevLink = null;
+                var nextLink = null;
+                data.links.forEach(element => {
+                    if(element.rel === 'prev')
+                        prevLink = element.href;
+                    else if(element.rel === 'next')
+                        nextLink = element.href;
+                });
+                
+                this.setState({
+                    companyList: data.data == null?[]:data.data, // Question mark conditional used in case search returns no results
+                    prev: prevLink,
+                    next: nextLink
+                }); 
             }
         });
     }
@@ -62,7 +76,41 @@ class MainScreen extends Component {
             } else if (res.statusCode !== 200) { // Then not successful
                console.log('Status:', res.statusCode);
             } else { // If successful, update state accordingly
-                this.setState({companyList: [data]});
+                this.setState({
+                    companyList: [data],
+                    prev: null, // Organization number is unique so it returns one or zero results, therefore no paginators
+                    next: null
+                });
+            }
+        });
+    }
+
+    switchPage(toNext){
+        request.get({
+            url: toNext?this.state.next:this.state.prev, // If toNext is set to true, go to next page, else go to previous page
+            json: true,
+            headers: {'User-Agent': 'request'}
+        }, 
+        (err, res, data) => {
+            if (err) {
+                console.log('Error:', err);
+            } else if (res.statusCode !== 200) { // Then not successful
+               console.log('Status:', res.statusCode);
+            } else { // If successful, update state accordingly
+                var prevLink = null;
+                var nextLink = null;
+                data.links.forEach(element => {
+                    if(element.rel === 'prev')
+                        prevLink = element.href;
+                    else if(element.rel === 'next')
+                        nextLink = element.href;
+                });
+
+                this.setState({
+                    companyList: data.data == null?[]:data.data, // Question mark conditional used in case search returns no results
+                    prev: prevLink,
+                    next: nextLink
+                }); 
             }
         });
     }
@@ -70,10 +118,10 @@ class MainScreen extends Component {
     render() {
         return (
         <div>
-            <h1>Skriv inn navn eller organisasjonsnummer</h1>
-            <input type='text' onChange={this.incrementalSearch}/>
+            <h1 className='searchInstruction'>Skriv inn navn eller organisasjonsnummer</h1>
+            <input className='searchField' type='text' onChange={this.incrementalSearch}/>
             {this.state.companyList.map(element => 
-            <details>
+            <details className='companyListing'>
                 <summary>{element.navn}</summary>  
                 <ul>              
                     {element.organisasjonsnummer !== undefined && <li>Organisasjonsnummer: {element.organisasjonsnummer} </li>}
@@ -84,6 +132,10 @@ class MainScreen extends Component {
                     {element.kontaktinformasjon && element.kontaktinformasjon.hjemmeside !== undefined && <li>Hjemmeside: {element.kontaktinformasjon.hjemmeside} </li>}
                 </ul>
             </details>)}
+            <div className='paginator'>
+                {this.state.prev !== null && <button onClick={() => this.switchPage(false)} className='paginatorButton'>&larr;</button>}
+                {this.state.next !== null && <button onClick={() => this.switchPage(true)} className='paginatorButton'>&rarr;</button>}
+            </div>
         </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainScreen from './MainScreen';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<MainScreen />, document.getElementById('root'));
